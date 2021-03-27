79f90cc1977617b0f7c05112335a593cb5fe6200
Jaagrav <jaagravseal03@gmail.com>
2021-03-27 20:35:47 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+// Importing Local components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./components/Home";
+
+// Importing Local Pages
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+
+          <Redirect to="/home" />
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
