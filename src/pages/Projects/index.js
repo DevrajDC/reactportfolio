36d3901f94930be4f1e344141fcb6ac288554d0d
DevrajDC <devrajchatribin9978@gmail.com>
2021-03-27 20:49:01 +0530

    added projects page

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,490 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <>
+      {/* // <!-- Projects Section --> */}
+      <section id="projects-section">
+        <div class="section-heading-text projects-head">PROJECTS</div>
+        <div class="grid-filter-btns container">
+          <span class="filter selected" project-type="">
+            All
+          </span>
+          <span class="filter" project-type="webdev">
+            Web Dev
+          </span>
+          <span class="filter" project-type="uiux">
+            UI/UX Design
+          </span>
+        </div>
+        <div class="container">
+          <div class="grid projects-grid">
+            <div class="item" project-type="uiux">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/projects/MUSIC%20APP%20-%20DARK.png"
+                      alt="Music App Design"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Music App Design</h4>
+                      <p class="secondary-text">
+                        This is a music app design made in figma and mockup
+                        designed in photoshop.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item" project-type="uiux">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/projects/Mobile%20App%20Design.png"
+                      alt="Chat App Design"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Chat App Design</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item" project-type="webdev">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/digitalart-DC.png"
+                      alt="My Portrait"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">My Portrait</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item" project-type="webdev">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/projects/tindog.PNG"
+                      alt="Tindog Web"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Tindog Web</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item" project-type="uiux">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/projects/colors-dc.PNG"
+                      alt="Colors - Color Pallete for Designers!"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">
+                        Colors - Color Pallete for Designers!
+                      </h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item" project-type="webdev">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/projects/Poler%20-%20create%20polls.PNG"
+                      alt="Poler - Poll maker"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Poler - Poll maker</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* // <!-- Artworks Section --> */}
+      <section id="artworks-section">
+        <div class="container">
+          <div class="section-heading-text artworks-head">ARTWORKS</div>
+          <div class="grid artworks-grid">
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/rashmika.png"
+                      alt="Rashmika"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Rashmika</h4>
+                      <p class="secondary-text">
+                        <b>Actress:</b> Rashmika Mandanna <b>Tools:</b> Adobe
+                        Illustrator + Photoshop
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/saloni.jpg"
+                      alt="Saloni"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Saloni</h4>
+                      <p class="secondary-text">
+                        <b>Tools:</b> Adobe Illustrator + Photoshop
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/modelface.jpg"
+                      alt="Model Face"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Model Face</h4>
+                      <p class="secondary-text">
+                        <b>Tools:</b> Adobe Illustrator + Photoshop
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/PRIYANKA%20FINAL.png"
+                      alt="Priyanka"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Priyanka</h4>
+                      <p class="secondary-text">
+                        <b>Tools:</b> HB,2B,6B Pencils & Black ball pen
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/medicinalplants.jpg"
+                      alt="Medicinal Plants"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Medicinal Plants</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/art/eyes2.jpg"
+                      alt="Eyes"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Eyes</h4>
+                      <p class="secondary-text">
+                        Designed a prototype for a messaging application named
+                        "Chatr" using Adobe XD Software.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* // <!-- Activities Section --> */}
+      <section id="activities-section">
+        <div class="container">
+          <div class="section-heading-text artworks-head">ACTIVITIES</div>
+          <div class="grid activities-grid">
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/activities/line-follower1.JPG"
+                      alt="Line Follower Robot"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Line Follower Robot</h4>
+                      <p class="secondary-text">
+                        Line follower is an autonomous robot which follows black
+                        line. Robot must be able to detect particular line and
+                        keep following it.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/activities/catapult.JPG"
+                      alt="Catapult"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Catapult</h4>
+                      <p class="secondary-text">
+                        Secured first place. Catapult was tested in different
+                        situations like shooting from varying distances and had
+                        to aim at targets.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/activities/photography1.jpg"
+                      alt="Photography"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Photography</h4>
+                      <p class="secondary-text">
+                        Secured first place in college level event called
+                        Photology. We had to click photo inside the college
+                        campus only.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/activities/bridge-building.jpg"
+                      alt="Bridge Building"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">Bridge Building</h4>
+                      <p class="secondary-text">
+                        Got selected in top 3. We had to make Bridge using only
+                        50 straws and a cello-tape. Weights were added and my
+                        brige could handle 1.7kg weight.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+              <div class="item-content">
+                <div class="project">
+                  <div class="project-image">
+                    <img
+                      src="https://devraj-chatribin.netlify.app/images/activities/car-lifting-machine.JPG"
+                      alt="Portable Car Lifting Mechanism"
+                      class="project-image-file"
+                    />
+                  </div>
+                  <div class="project-info-overlay">
+                    <div class="overlay-details">
+                      <h4 class="project-name">
+                        Portable Car Lifting Mechanism
+                      </h4>
+                      <p class="secondary-text">
+                        Made a portable car lifting mechanism which can be used
+                        to work on cars and also if a car is needed to move from
+                        one place to another.
+                      </p>
+                      <a href="http://" class="gradient-text">
+                        Open →
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Projects;
